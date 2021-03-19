4f9b840e8dca21bad919706a7bcbf901dc54b409
Arielle Loring <arieloring@gmail.com>
2021-03-18 18:45:15 -0600

    add form functionality

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Form, {TYPES} from 'react-native-basic-form';
+
 
 export default function App() {
+
+  const options = [
+  {label:'1', value:1},
+  {label:'2', value:2},
+  {label:'3', value:3},
+  ]
+  const initialData = {
+    'age': 2
+  }
+
+  const fields = [
+    {name: 'age', label: 'Age', required: true, type: TYPES.Dropdown, options: options}
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Form
+        title={"Generate"}
+        fields={fields}
+        initialData={initialData}
+
+      />
     </View>
   );
 }
---
+++ activityData.js
@@ -0,0 +1,8 @@
+// age, indoor/outdoor, cost, effort, weather
+const activityData = [
+  {
+    
+  }
+]
+
+export default activityData;
